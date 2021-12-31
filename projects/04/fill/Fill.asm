ef63543a7b82a6d8602d34cc581d305a87c8a4d7
Peter Davison <davison.pl@gmail.com>
2021-12-31 11:59:12 -0500

    Completed mult, first attempt at fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,95 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@3
+D=A
+@numrows
+M=D
+@white
+M=0
+D=A
+@status
+M=D
+@black
+M=-1
+D=A
+@notstatus
+M=D
+
+(MAINLOOP)
+    // check for keyboard input if none, do nothing
+    @KBD
+    D=M
+    @MAINLOOP
+    D;JEQ
+
+    // paint everything
+    @PAINT
+    0;JMP
+    (ENDPAINT)
+    
+    // flip status
+    @FLIPSTATUS
+    0;JMP
+    (ENDFLIPSTATUS)
+
+    @MAINLOOP
+    0;JMP
+
+// paint subroutine
+(PAINT)
+    @i
+    M=0
+
+    // val stores the value that we paint each row
+    @notstatus
+    A=M
+    D=M
+    @val
+    M=D
+
+    (PLOOP)
+        // if i == numrows stop
+        @i
+        D=M
+	@numrows
+	D=D-M
+        @ENDPAINT
+        D;JEQ
+
+        // RAM[SCREEN+i] = val
+	@SCREEN
+	D=A
+	@i
+	D=D+M
+	@R0
+	M=D   // address of current screen row stored in R0
+	@val
+	D=M
+	@R0
+	M=D
+
+	// i++
+	@i
+	M=M+1
+
+	@PLOOP
+	0;JMP
+
+// flip status subroutine
+(FLIPSTATUS)
+    @status
+    D=M
+    @R0
+    M=D
+    @notstatus
+    D=M
+    @status
+    M=D
+    @R0
+    D=M
+    @notstatus
+    M=D
+    @ENDFLIPSTATUS
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,42 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// R2 = 0
+// R3 = R1
+// (LOOP)
+// if R3 <= 0 jump to ENDLOOP
+// R2 += R0
+// R3--
+// jump to loop
+// (ENDLOOP)
+// jump to ENDLOOP
+    
+    @R2
+    M=0
+    @R1
+    D=M
+    @R3
+    M=D
+
+(LOOP)
+    // condition
+    @R3
+    D=M
+    @END
+    D;JLE
+
+    // action
+    @R0
+    D=M
+    @R2
+    M=M+D
+
+    @R3
+    M=M-1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
